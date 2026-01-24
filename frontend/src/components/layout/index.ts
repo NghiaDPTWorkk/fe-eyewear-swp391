f6398bbf1246a7b9a6db766f37082fd485efe01a
Tr.Nghia <t.nghia2112279@gmail.com>
2026-01-24 20:31:53 +0700

    KAN-41 refactor: fix folder

--- frontend/src/components/layout/index.ts
+++ frontend/src/components/layout/index.ts
@@ -1,4 +1,4 @@
 export { Footer } from './Footer'
 export { HeaderStaff } from './HeaderStaff'
 export { Container } from './container'
-export type { ContainerProps } from './container'
+export type { ContainerProps } from './container' // test

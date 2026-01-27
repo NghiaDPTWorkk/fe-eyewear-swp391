f4d566eec71157eb6bc4795f523c84b8fb3b8a91
Tran Hong Gam <honggamdx2@gmail.com>
2026-01-27 21:55:55 +0700

    KAN-63 fix: ui conflict header component from merge

--- frontend/src/components/layout/staff/staff-core/main-layout/StaffMainLayout.tsx
+++ frontend/src/components/layout/staff/staff-core/main-layout/StaffMainLayout.tsx
@@ -3,6 +3,7 @@ import { Outlet } from 'react-router-dom'
 import { useLayoutStore } from '@/store/layout.store'
 import { cn } from '@/lib/utils'
 import { StaffHeader } from '../header'
+import { Container } from '@/shared/components/ui'
 
 interface StaffMainLayoutProps {
   sidebar: ReactNode
@@ -40,7 +41,13 @@ export function StaffMainLayout({
           sidebarCollapsed ? 'lg:ml-20' : 'lg:ml-64'
         )}
       >
-        <StaffHeader containerWidth={headerContainerWidth} left={headerLeft} right={headerRight} />
+        <Container>
+          <StaffHeader
+            containerWidth={headerContainerWidth}
+            left={headerLeft}
+            right={headerRight}
+          />
+        </Container>
 
         <main className={cn('h-full overflow-auto', mainClassName)}>
           <Outlet />

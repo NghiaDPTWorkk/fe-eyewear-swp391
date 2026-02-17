e38b0de8846f2fa93282bba724fa286fd1a28e80
ckjn0411 <uttrinh4115@gmail.com>
2026-02-17 23:24:16 +0700

    KAN-999 fix: vite config proxy api url

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,31 +1,35 @@
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://34.92.192.47:5000',
-        changeOrigin: true
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+      port: 3000,
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL || 'http://localhost:5000',
+          changeOrigin: true
+        }
       }
-    }
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@/api': path.resolve(__dirname, './src/api'),
-      '@/store': path.resolve(__dirname, './src/store'),
-      '@/shared': path.resolve(__dirname, './src/shared'),
-      '@/features': path.resolve(__dirname, './src/features'),
-      '@/lib': path.resolve(__dirname, './src/lib'),
-      '@/routes': path.resolve(__dirname, './src/routes'),
-      '@/pages': path.resolve(__dirname, './src/pages'),
-      '@/components': path.resolve(__dirname, './src/components')
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+        '@/api': path.resolve(__dirname, './src/api'),
+        '@/store': path.resolve(__dirname, './src/store'),
+        '@/shared': path.resolve(__dirname, './src/shared'),
+        '@/features': path.resolve(__dirname, './src/features'),
+        '@/lib': path.resolve(__dirname, './src/lib'),
+        '@/routes': path.resolve(__dirname, './src/routes'),
+        '@/pages': path.resolve(__dirname, './src/pages'),
+        '@/components': path.resolve(__dirname, './src/components')
+      }
     }
   }
 })

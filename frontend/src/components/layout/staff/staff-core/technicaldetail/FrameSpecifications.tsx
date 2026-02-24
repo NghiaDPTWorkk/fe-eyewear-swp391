cc9b6594d890cebeae70a223517344ca04d52ce0
Tran Hong Gam <honggamdx2@gmail.com>
2026-02-24 14:37:10 +0700

    KAN-119 fix: color frmae specification

--- frontend/src/components/layout/staff/staff-core/technicaldetail/FrameSpecifications.tsx
+++ frontend/src/components/layout/staff/staff-core/technicaldetail/FrameSpecifications.tsx
@@ -15,15 +15,15 @@ const FrameSpecifications = ({
 }: FrameProps) => {
   return (
     <div className="flex flex-col md:flex-row gap-6">
-      <div className="w-55 h-55 md:w-1/3 bg-gray-50 rounded-lg p-4 flex justify-center">
+      <div className="w-50 h-50 md:w-1/3 bg-gray-50 rounded-lg py-3 flex justify-center">
         <img src={imageSrc} alt="Frame" className="max-w-full h-auto object-contain" />
       </div>
 
       <div className="w- md:w-fit grid grid-cols-2 gap-x-12 gap-y-4">
         {data.map((item, idx) => (
           <div key={idx} className="min-w-[120px] py-3 px-8">
-            <p className="text-xs text-gray-500">{item.key}</p>
-            <p className="text-sm font-semibold text-gray-900">{item.value}</p>
+            <p className="text-sm text-gray-500">{item.key}</p>
+            <p className="text-md font-semibold text-gray-900">{item.value}</p>
           </div>
         ))}
       </div>

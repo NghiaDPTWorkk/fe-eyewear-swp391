d799f8377c2f5e65d4f912fad2208f6b3c8b4a35
Tran Hong Gam <honggamdx2@gmail.com>
2026-02-25 15:38:37 +0700

    KAN-119 fix: fix frame quantity for manufacturing order

--- frontend/src/components/layout/staff/staff-core/technicaldetail/FrameSpecifications.tsx
+++ frontend/src/components/layout/staff/staff-core/technicaldetail/FrameSpecifications.tsx
@@ -13,12 +13,12 @@ interface FrameProps {
 const FrameSpecifications = ({
   data,
   imageSrc = 'https://kinhmats.com/wp-content/uploads/2024/05/gentle-monster-jennie-donut-bun-01.webp',
-  quantity
+  quantity = 1
 }: FrameProps) => {
   return (
     <div>
       <div className="text-sm text-mint-800 mb-4 font-semibold ps-2">
-        Quantity: <span className="text-gray-900">{quantity ?? '-'}</span>
+        Quantity: <span className="text-gray-900">{quantity}</span>
       </div>
       <div className="flex flex-col md:flex-row gap-6">
         <div className="w-50 h-50 md:w-1/3 bg-gray-50 rounded-lg py-3 flex justify-center">

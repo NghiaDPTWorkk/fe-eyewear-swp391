f227711f97ad7e0ea009f1b18cf61cdf341d5188
ckjn0411 <uttrinh4115@gmail.com>
2026-03-02 15:12:15 +0700

    KAN-111 fix: change to vnpay

--- frontend/src/shared/types/enums.ts
+++ frontend/src/shared/types/enums.ts
@@ -47,7 +47,6 @@ export type InvoiceStatus = (typeof InvoiceStatus)[keyof typeof InvoiceStatus]
 // Payment Enums
 export const PaymentMethodType = {
   COD: 'COD',
-  ZALAPAY: 'ZALAPAY',
   MOMO: 'MOMO',
   VNPAY: 'VNPAY'
 } as const

08af1bf88631c7d7491d07f0449f99a5af196e25
Tran Hong Gam <honggamdx2@gmail.com>
2026-03-16 18:12:35 +0700

    KAN-150 fix: ui dashboard admin

--- frontend/src/components/layout/staff/operation-staff/inventoryplantable/InventoryTableHeader.tsx
+++ frontend/src/components/layout/staff/operation-staff/inventoryplantable/InventoryTableHeader.tsx
@@ -2,22 +2,22 @@ export const InventoryTableHeader = () => {
   return (
     <thead>
       <tr className="bg-white border-b border-slate-100">
-        <th className="px-6 py-4 text-[11px] font-bold text-gray-400 uppercase tracking-widest">
+        <th className="px-6 py-4 text-[11px] font-bold text-neutral-500 uppercase tracking-wider">
           SKU
         </th>
-        <th className="px-6 py-4 text-[11px] font-bold text-gray-400 uppercase tracking-widest">
+        <th className="px-6 py-4 text-[11px] font-bold text-neutral-500 uppercase tracking-wider">
           MANAGER
         </th>
-        <th className="px-6 py-4 text-[11px] font-bold text-gray-400 uppercase tracking-widest text-center">
+        <th className="px-6 py-4 text-[11px] font-bold text-neutral-500 uppercase tracking-wider">
           TARGET QTY
         </th>
-        <th className="px-6 py-4 text-[11px] font-bold text-gray-400 uppercase tracking-widest text-center">
+        <th className="px-6 py-4 text-[11px] font-bold text-neutral-500 uppercase tracking-wider">
           STATUS
         </th>
-        <th className="px-6 py-4 text-[11px] font-bold text-gray-400 uppercase tracking-widest text-center">
+        <th className="px-6 py-4 text-[11px] font-bold text-neutral-500 uppercase tracking-wider">
           CREATED AT
         </th>
-        <th className="px-6 py-4 text-[11px] font-bold text-gray-400 uppercase tracking-widest text-center">
+        <th className="px-6 py-4 text-[11px] font-bold text-neutral-500 uppercase tracking-wider">
           ACTION
         </th>
       </tr>
